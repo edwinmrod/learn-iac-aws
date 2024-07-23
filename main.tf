1e6deca8c0716fc54de84a05c4e58f5911204493
edwinRod2 <erodr28@bancodebogota.com.co>
2024-07-23 18:53:05 -0500

    change configuration terraform

--- main.tf
+++ main.tf
@@ -1,14 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
-  region     = "us-east-1"
+  version = "~> 4.0"
+  region  = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
