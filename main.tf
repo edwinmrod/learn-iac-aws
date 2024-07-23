7017bb64560ba59fc66e4ebf9bcf690e04802e1e
edwinRod2 <erodr28@bancodebogota.com.co>
2024-07-23 18:57:36 -0500

    main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.56.0"
+    }
+  }
+}
+
+
 provider "aws" {
-  version = "~> 4.0"
-  region  = "us-east-1"
+  region     = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
