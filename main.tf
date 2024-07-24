4dc9932531124ee793dfead2d1e124556bf16ede
edwinRod2 <erodr28@bancodebogota.com.co>
2024-07-24 11:39:40 -0500

    add bucket to manage the states terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "terraform-state-recov"
+    key    = "estado/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
