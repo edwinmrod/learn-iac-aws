a5e1216c45d155e2d30513da88008c01ae1c8e84
edwinRod2 <erodr28@bancodebogota.com.co>
2024-07-24 09:20:26 -0500

    ininstall version in gitflow

--- main.tf
+++ main.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.56.0"
+      version = "~>4.0"
     }
   }
 }
-
 
 provider "aws" {
   region     = "us-east-1"
